f98750137592d5577c1cd5c13d4404527ed4c755
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-28 11:25:27 +0000

    set debug to false

--- vrimages/settings.py
+++ vrimages/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-michaelobadun-blogger-8dwkwpd2oxm.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
