39b7e49d69b58eabede94371a94aa7964cc3f7c7
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-28 11:21:55 +0000

    created superuser and set debug to true

--- vrimages/settings.py
+++ vrimages/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "SECRET_KEY"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-michaelobadun-blogger-8dwkwpd2oxm.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
