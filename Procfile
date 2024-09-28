f8490f58039e61283119e13d829b076dfc79a688
Michael Obadun <r65esxbn@students.codeinstitute.net>
2024-09-28 10:56:02 +0000

    installed gunicorn and added it to requirements.txt. Created procfile and added herokuapp.com to allowed host. I also set debug to false

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn vrimages.wsgi
--- vrimages/settings.py
+++ vrimages/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = 'django-insecure-!14g_xx_f-42bt-(t9q-(_vng4atc-8y+)_7t*gldnrl&j70&q
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-michaelobadun-blogger-8dwkwpd2oxm.ws.codeinstitute-ide.net']
+ALLOWED_HOSTS = ['8000-michaelobadun-blogger-8dwkwpd2oxm.ws.codeinstitute-ide.net',
+                '.herokuapp.com']
 
 
 # Application definition
